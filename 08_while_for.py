31d03114cdaacd251711c7e9e000f18a90935fd9
944851899@qq.om <944851899@qq.com>
2021-09-12 21:18:08 +0800

    add code while and for

---
+++ 08_while_for.py
@@ -0,0 +1,13 @@
+# expression1 if conditions1 else expression2 for item in items 
+# 以上的表达式相当于以下代码
+
+items = list()
+
+for item in items:
+    if conditions1:
+        expressions1
+    else:
+        expressions2
+
+# 如果没有else，则需要写成如下代码
+# expression for item in items if condition
